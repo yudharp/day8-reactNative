8b8bc565687a2fa1ce9c8799745ad5062937e388
zackia <Zainzakia99@gmail.com>
2024-02-07 11:21:14 +0700

    post data

--- App.js
+++ App.js
@@ -1,21 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const AddPost = () => {
+    const [usertitle, setUsertitle] = useState('');
+    const [userbody, setUserbody] = useState('');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    const handleAddPost = async () => {
+        try {
+            const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
+            title: usertitle,
+            body: userbody,
+        });
+
+            console.log('News added:', response.data);
+        } catch (error) {
+            console.error('Error adding news', error);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Add news</Text>
+            <TextInput
+                style={styles.input}
+                placeholders="Masukkan title"
+                value={usertitle}
+                onChangeText={text => setUsertitle(text)}
+            />
+            <TextInput
+                style={styles.input}
+                placeholders="Masukkan body"
+                value={userbody}
+                onChangeText={text => setUserbody(text)}
+            />
+            <TouchableOpacity style={styles.addButton} onPress={handleAddPost}>
+                <Text style={styles.buttonText}>Add news</Text>
+            </TouchableOpacity>
+        </View>
+    )
+};
+
+const styles= StyleSheet.create ({
+    container:{
+        flex : 1,
+        padding : 16,
+    },
+    title:{
+        fontSize: 20,
+        fontWeight:'bold',
+        marginBottom : 16,
+    },
+    usertitle:{
+        marginBottom:16,
+        borderWidth: 1,
+        borderColor:'#ddd',
+        padding : 10,
+        borderRadius:8,
+    },
+    userbody:{
+        fontSize:16,
+        fontWeight:'bold',
+        marginBottom:8,
+    },
+})
+
+export default AddPost;
--- src/screens/postt.js
+++ src/screens/postt.js
@@ -1,42 +1,67 @@
 import React, { useState } from "react";
-import { View, text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import axios from 'axios';
 
-const AddUser = () => {
+const AddPost = () => {
     const [usertitle, setUsertitle] = useState('');
     const [userbody, setUserbody] = useState('');
 
-    const handleAddUser = async () => {
+    const handleAddPost = async () => {
         try {
             const response = await axios.post('https://jsonplaceholder.typicode.com/posts', {
             title: usertitle,
             body: userbody,
         });
 
-            console.log('User added:', response.data);
+            console.log('News added:', response.data);
         } catch (error) {
-            console.error('Error adding user', error);
+            console.error('Error adding news', error);
         }
     };
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Add user</Text>
+            <Text style={styles.title}>Add news</Text>
             <TextInput
                 style={styles.input}
-                placeholders="Masukkan nama"
+                placeholders="Masukkan title"
                 value={usertitle}
                 onChangeText={text => setUsertitle(text)}
             />
             <TextInput
                 style={styles.input}
-                placeholders="Masukkan email"
+                placeholders="Masukkan body"
                 value={userbody}
                 onChangeText={text => setUserbody(text)}
             />
-            <TouchableOpacity style={styles.addButton} onPress={handleAddUser}>
-                <Text style={styles.buttonText}>Add user</Text>
+            <TouchableOpacity style={styles.addButton} onPress={handleAddPost}>
+                <Text style={styles.buttonText}>Add news</Text>
             </TouchableOpacity>
         </View>
     )
 };
+
+const style= StyleSheet.create ({
+    container:{
+        flex : 1,
+        padding : 16,
+    },
+    title:{
+        fontSize: 20,
+        fontWeight:'bold',
+        marginBottom : 16,
+    },
+    userItem:{
+        marginBottom:16,
+        borderWidth: 1,
+        borderColor:'#ddd',
+        padding : 10,
+        borderRadius:8,
+    },
+    userName:{
+        fontSize:16,
+        fontWeight:'bold',
+        marginBottom:8,
+    },
+})
+export default AddPost;
